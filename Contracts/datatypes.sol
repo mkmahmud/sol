cef372db117c4d3ef5b064188267e0df36ffd310
mkmahmud <mahmudulhasanmk434@gmail.com>
2025-02-20 18:00:20 +0600

    data

--- Contracts/datatypes.sol
+++ Contracts/datatypes.sol
@@ -2,6 +2,15 @@
 pragma solidity ^0.8.4;
 
 contract dataTypesInSol {
-    int public  a = type(int).min;
+    int public  a = type(int).min; 
+
+    // Byte is more gas efficiency
+
+    bytes1 public aa;
+
+    bytes1 public bb = 0x87;
+
+    address public hey;
+    
 
 }
